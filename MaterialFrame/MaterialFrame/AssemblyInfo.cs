241e7e46d68d221311901df889b3eb962e6d77a5
roubachof <jm.alfonsi@gmail.com>
2020-11-17 12:38:56 +0100

    update to xf 4.8

--- MaterialFrame/MaterialFrame/AssemblyInfo.cs
+++ MaterialFrame/MaterialFrame/AssemblyInfo.cs
@@ -8,13 +8,13 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyTitle("Sharpnado.MaterialFrame")]
-[assembly: System.Reflection.AssemblyCompany("Sharpnado")]
 [assembly: System.Reflection.AssemblyDescription("Modern Frame supporting theming such as Blur styles, Acrylic style, Dark/Light mo" +
     "des for Xamarin.Forms (Android, iOS, UWP).")]
+[assembly: System.Reflection.AssemblyCompany("Sharpnado")]
 [assembly: System.Reflection.AssemblyProduct("MaterialFrame")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright © Sharpnado 2020")]
-[assembly: System.Reflection.AssemblyVersion("1.1.3")]
 [assembly: System.Reflection.AssemblyFileVersion("1.1.3")]
+[assembly: System.Reflection.AssemblyVersion("1.1.3")]
+[assembly: System.Reflection.AssemblyTitle("Sharpnado.MaterialFrame")]
 
 
