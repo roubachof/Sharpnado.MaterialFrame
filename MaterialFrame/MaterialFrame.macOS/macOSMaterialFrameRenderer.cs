82b1bded8168df36196f38aab37ad530136d4139
nor0x <nor0x@users.noreply.github.com>
2021-06-30 08:39:41 +0200

    acrylic effect fixed in macOS renderer
    frame of _intermediateLayer fixed

--- MaterialFrame/MaterialFrame.macOS/macOSMaterialFrameRenderer.cs
+++ MaterialFrame/MaterialFrame.macOS/macOSMaterialFrameRenderer.cs
@@ -346,7 +346,7 @@ namespace Sharpnado.MaterialFrame.macOS
             {
                 InternalLogger.Debug(Tag, () => "UpdateLayerBounds()");
 
-                _intermediateLayer.Frame = new CGRect(0, 2, Element.Width, Element.Height - 2);
+                _intermediateLayer.Frame = new CGRect(0, 0, Element.Width, Element.Height - 2);
                 _intermediateLayer.RemoveAllAnimations();
             }
         }
